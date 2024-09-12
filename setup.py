d02e89f3fc8d1790f47d44a97a116221d888d2b9
tsutterley <tsutterl@uw.edu>
2024-09-12 15:58:09 -0700

    use semantic versioning

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+# get version
+with open('version.txt', mode='r', encoding='utf8') as fh:
+    fallback_version = fh.read()
+
+# list of all scripts to be included with package
+scripts=[os.path.join('scripts',f) for f in os.listdir('scripts') if f.endswith('.py')]
+
+# semantic version configuration for setuptools-scm
+setup_requires = ["setuptools_scm"]
+use_scm_version = {
+    "relative_to": __file__,
+    "local_scheme": "node-and-date",
+    "version_scheme": "python-simplified-semver",
+    "fallback_version":fallback_version,
+}
+
+setup(
+    name='pointAdvection',
+    use_scm_version=use_scm_version,
+    scripts=scripts,
+)
